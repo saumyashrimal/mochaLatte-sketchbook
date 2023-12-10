0d511cf840ae158e3d34d0ed0f271b2489a28182
Samyak15 <samyakjainudaipur@gmail.com>
2023-12-11 01:32:43 +0530

    added communicaiton between toolbox and menu via redux toolkit

---
+++ src/Components/Menu/index.js
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
+import styles from './styles.module.css';
+import { menuItemClick, actionItemClick } from '@/slice/menuSlice';
+import { MENUITEMS } from '@/contants';
+import cx from 'classnames';
+import {useDispatch, useSelector} from 'react-redux';
+export default function index() {
+    const dispatch = useDispatch();
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+    const handleMenuClick = (itemName) => {
+        dispatch(menuItemClick(itemName));
+    }
+  return (
+    <div className={styles.menuContainer}>
+        <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENUITEMS.PENCIL})} onClick={() => handleMenuClick(MENUITEMS.PENCIL)}>
+            <FontAwesomeIcon icon={faPencil} className={styles.icon}/>
+        </div>
+        <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENUITEMS.ERASER})} onClick={() => handleMenuClick(MENUITEMS.ERASER)}>
+            <FontAwesomeIcon icon={faEraser} className={styles.icon} />
+        </div>
+        <div className={styles.iconWrapper} onClick={() => handleMenuClick(MENUITEMS.UNDO)}>
+            <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
+        </div>
+        <div className={styles.iconWrapper} onClick={() => handleMenuClick(MENUITEMS.REDO)}>
+            <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
+        </div>
+        <div className={styles.iconWrapper} onClick={() => handleMenuClick(MENUITEMS.DOWNLOAD)}>
+            <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
+        </div>
+    </div>
+  )
+}
---
+++ src/contants.js
@@ -0,0 +1,24 @@
+
+const COLORS  = {
+    BLACK: "black",
+    RED: "red",
+    GREEN: "green",
+    BLUE: "blue",
+    ORANGE: "orange",
+    YELLOW: "yellow",
+    WHITE: "white"
+}
+
+
+const MENUITEMS = {
+    PENCIL: 'PENCIL',
+    ERASER: 'ERASER',
+    UNDO: 'UNDO',
+    REDO: 'REDO',
+    DOWNLOAD: 'DOWNLOAD'
+}
+
+
+
+export {COLORS, MENUITEMS};
+
